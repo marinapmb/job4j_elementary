929e12d83e9606ba432d8267663df15615b7fd17
Stitch <pesterevamb@gmail.com>
2025-10-05 11:53:29 +0300

    Модульные тесты [#505277]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -7,8 +7,8 @@ import static org.assertj.core.api.Assertions.*;
 class ConverterTest {
 
     @Test
-    void whenConvert140RblThen2Euro() {
-        float input = 140;
+    void whenConvert180RblThen2Euro() {
+        float input = 180;
         float expected = 2;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
@@ -34,9 +34,9 @@ class ConverterTest {
     }
 
     @Test
-    void whenConvert180RblThen3Dollar() {
-        float input = 180;
-        float expected = 3;
+    void whenConvert150RblThen2Dollar() {
+        float input = 150;
+        float expected = 2;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));

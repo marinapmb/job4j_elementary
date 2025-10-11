6dac1cdbc65ffaad8fe1f5a9433756b38f277732
Stitch <pesterevamb@gmail.com>
2025-10-11 22:22:30 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,16 +1,13 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
-import ru.job4j.converter.Converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.BDDAssertions.withPrecision;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
 
     @Test
-    void whenStart0Finish0Then0() {
+    void whenSumStart0Finish0Then0() {
         int start = 0;
         int finish = 0;
         int actual = Counter.sum(start, finish);
@@ -19,7 +16,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart1Finish5Then15() {
+    void whenSumStart1Finish5Then15() {
         int start = 1;
         int finish = 5;
         int actual = Counter.sum(start, finish);
@@ -28,7 +25,7 @@ class CounterTest {
     }
 
     @Test
-    void whenStart20Finish5Then0() {
+    void whenSumStart20Finish5Then0() {
         int start = 20;
         int finish = 5;
         int actual = Counter.sum(start, finish);
@@ -37,11 +34,47 @@ class CounterTest {
     }
 
     @Test
-    void whenStartMinus10FinishMinus5ThenMinus45() {
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumStartMinus10FinishMinus5ThenMinus45() {
         int start = -10;
         int finish = -5;
         int actual = Counter.sum(start, finish);
         int expected = -45;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStart1FinishThen6() {
+        int start = 1;
+        int finish = 6;
+        int actual = Counter.sumByEven(start, finish);
+        int expected = 12;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStart0Finish0Then0() {
+        int start = 0;
+        int finish = 0;
+        int actual = Counter.sumByEven(start, finish);
+        int expected = 0;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersStartMinus5Finish2Then0() {
+        int start = -5;
+        int finish = 2;
+        int actual = Counter.sumByEven(start, finish);
+        int expected = -4;
         assertThat(actual).isEqualTo(expected);
     }
 }

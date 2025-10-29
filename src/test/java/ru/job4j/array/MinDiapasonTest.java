97d973847a8aad9434f7f2f5e66278fccb1a8107
Stitch <pesterevamb@gmail.com>
2025-10-29 22:03:50 +0300

    Поиск минимума в диапазоне [#505239]

--- src/test/java/ru/job4j/array/MinDiapasonTest.java
+++ src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MinDiapasonTest {
     @Test

8b0e0edc52612f5cbe68f0fd528a7eda0a267fa0
Stitch <pesterevamb@gmail.com>
2025-11-04 19:00:44 +0300

    Выигрышные комбинации в сокобан [#505248 #554201]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -33,13 +33,10 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        char[] diag = extractDiagonal(board);
         for (int i = 0; i < board.length; i++) {
-            if (diag[i] == 'X') {
-                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i)))  {
                     result = true;
                     break;
-                }
             }
         }
         return result;
